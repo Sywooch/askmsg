92886ed8d815c039fcff02628f0eb60be2b989aa
Victor <devmosedu@yandex.ru>
2015-03-05 14:32:14 +0300

    Add migration with tag tables

---
+++ migrations/m150305_111647_add_tags_tables.php
@@ -0,0 +1,59 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m150305_111647_add_tags_tables extends Migration
+{
+    public function up()
+    {
+        $tableOptions = null;
+        $tableOptionsMyISAM = null;
+        if ($this->db->driverName === 'mysql') {
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=InnoDB';
+            $tableOptionsMyISAM = 'CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=MyISAM';
+        }
+
+        $this->createTable('{{%tags}}', [
+            'tag_id' => Schema::TYPE_PK,
+            'tag_active' => Schema::TYPE_SMALLINT . ' DEFAULT 1',
+            'tag_title' => Schema::TYPE_STRING,
+        ], $tableOptionsMyISAM);
+
+        $this->createIndex('idx_tags_title', '{{%tags}}', 'tag_title');
+
+        $this->createTable('{{%msgtags}}', [
+            'mt_id' => Schema::TYPE_PK,
+            'mt_msg_id' => Schema::TYPE_INTEGER,
+            'mt_tag_id' => Schema::TYPE_STRING,
+        ], $tableOptionsMyISAM);
+
+        $this->createIndex('idx_msgtags_msgid', '{{%msgtags}}', 'mt_msg_id');
+        $this->createIndex('idx_msgtags_tagid', '{{%msgtags}}', 'mt_tag_id');
+    }
+
+    public function down()
+    {
+//        return true;
+
+        $this->dropIndex('idx_msgtags_msgid', '{{%msgtags}}');
+        $this->dropIndex('idx_msgtags_tagid', '{{%msgtags}}');
+        $this->dropTable('{{%msgtags}}');
+
+        $this->dropIndex('idx_tags_title', '{{%tags}}');
+        $this->dropTable('{{%tags}}');
+
+        return true;
+    }
+    
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+    
+    public function safeDown()
+    {
+    }
+    */
+}
