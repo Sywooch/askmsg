855ce8e5cba6d8528687904e957560d55541be93
Victor <devmosedu@yandex.ru>
2015-04-09 12:29:07 +0300

    Change word export

--- views/message/export-doc.php
+++ views/message/export-doc.php
@@ -113,18 +113,19 @@ $aWidth = [
     3700,
     7300
 ];
-
+/*
 $aWidth1 = [
     $aWidth[0],
     $bLocal ? $aWidth[1] : array_reduce(array_slice($aWidth, 0, 2), function($carry, $item) { return $carry + $item;}, 0),
     $bLocal ? $aWidth[2] : array_reduce(array_slice($aWidth, 0, 3), function($carry, $item) { return $carry + $item;}, 0),
     $bLocal ? $aWidth[3] : array_reduce(array_slice($aWidth, 0, 4), function($carry, $item) { return $carry + $item;}, 0),
 ];
+*/
 
-insertText($table->addCell($aWidth1[0], $styleHeaderCell), '№' . "\r\n" . 'Дата', $fontHeaderStyle);
-insertText($table->addCell($aWidth1[1], $styleHeaderCell), 'Фамилия Имя Отчество' . "\r\n" . 'Контакты', $fontHeaderStyle);
-insertText($table->addCell($aWidth1[2], $styleHeaderCell), 'Тема' . "\r\n" . 'Учреждение', $fontHeaderStyle);
-insertText($table->addCell($aWidth1[3], $styleHeaderCell), 'Обращение', $fontHeaderStyle);
+insertText($table->addCell($aWidth[0], $styleHeaderCell), '№' . "\r\n" . 'Дата', $fontHeaderStyle);
+insertText($table->addCell($aWidth[1], $styleHeaderCell), 'Фамилия Имя Отчество' . "\r\n" . 'Контакты', $fontHeaderStyle);
+insertText($table->addCell($aWidth[2], $styleHeaderCell), 'Тема' . "\r\n" . 'Учреждение', $fontHeaderStyle);
+insertText($table->addCell($aWidth[3], $styleHeaderCell), 'Обращение', $fontHeaderStyle);
 // $table->addCell($n2sm, $styleCell)->addText(prepare('№' . "\r\n" . 'Дата'), $fontStyle);
 //$table->addCell($n5_2sm, $styleCell)->addText(prepare('Фамилия Имя Отчество' . "\r\n" . 'Контакты'), $fontStyle);
 //$table->addCell($n5_2sm, $styleCell)->addText(prepare('Тема' . "\r\n" . 'Учреждение'), $fontStyle);
@@ -136,10 +137,10 @@ for($page = 0; $page < $nPageCount; $page++) {
     $dataProvider->refresh();
     foreach($dataProvider->getModels() As $model) {
         $table->addRow();
-        insertText($table->addCell($aWidth1[0], $styleCell), $model->msg_id . "\r\n" . date("d.m.Y", strtotime($model->msg_createtime)) . "\r\n" . $model->flag->fl_sname, $fontStyle);
-        insertText($table->addCell($aWidth1[1], $styleCell), $model->getFullName() . "\r\n" . $model->msg_pers_email . "\r\n" . $model->msg_pers_phone . "\r\n\r\n" . (($model->msg_empl_id !== null) ? $model->employee->getFullName() : ''), $fontStyle);
-        insertText($table->addCell($aWidth1[2], $styleCell), ($model->subject ? ($model->subject->tag_title . "\r\n") : '') . $model->msg_pers_org, $fontStyle);
-        insertText($table->addCell($aWidth1[3], $styleCell), $model->msg_pers_text, $fontStyle);
+        insertText($table->addCell($aWidth[0], $styleCell), $model->msg_id . "\r\n" . date("d.m.Y", strtotime($model->msg_createtime)) . "\r\n" . $model->flag->fl_sname, $fontStyle);
+        insertText($table->addCell($aWidth[1], $styleCell), $model->getFullName() . "\r\n" . $model->msg_pers_email . "\r\n" . $model->msg_pers_phone . "\r\n\r\n" . (($model->msg_empl_id !== null) ? $model->employee->getFullName() : ''), $fontStyle);
+        insertText($table->addCell($aWidth[2], $styleCell), ($model->subject ? ($model->subject->tag_title . "\r\n") : '') . $model->msg_pers_org, $fontStyle);
+        insertText($table->addCell($aWidth[3], $styleCell), $model->msg_pers_text, $fontStyle);
 /*
         $table->addCell($n2sm)->addText(prepare($model->msg_id . "\r\n" . date("d.m.Y", strtotime($model->msg_createtime)) . "\r\n" . $model->flag->fl_sname));
         $table->addCell($n5_2sm)->addText(prepare($model->getFullName() . "\r\n" . $model->msg_pers_email . "\r\n" . $model->msg_pers_phone . "\r\n\r\n" . (($model->msg_empl_id !== null) ? $model->employee->getFullName() : '')));
