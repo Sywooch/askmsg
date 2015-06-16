e8404696085aac0e75985245d4233c4b6e0b124b
Victor <devmosedu@yandex.ru>
2015-06-16 15:02:56 +0300

    Поставлен восклицательный знак в ответе.

--- views/message/_formanswer.php
+++ views/message/_formanswer.php
@@ -110,7 +110,7 @@ $aFieldParam = [
 
     <?php
         if( $model->msg_answer == '' ) {
-            $model->msg_answer = 'Уважаемая(-ый) ' . $model->getShortName() . ".\nС уважением, " . Yii::$app->user->identity->getFullName() . '.';
+            $model->msg_answer = 'Уважаемая(-ый) ' . $model->getShortName() . "!\n\nС уважением, " . Yii::$app->user->identity->getFullName() . '.';
         }
 
         // Фокус на редактор помещаем
