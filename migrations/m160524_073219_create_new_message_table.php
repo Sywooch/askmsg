f0e7155f78a276a9e2390e6320056c3bbf52cc7b
Victor <devmosedu@yandex.ru>
2016-06-20 16:42:16 +0300

    Change table structure

--- migrations/m160524_073219_create_new_message_table.php
+++ migrations/m160524_073219_create_new_message_table.php
@@ -12,7 +12,9 @@ class m160524_073219_create_new_message_table extends Migration
         $this->createTable('{{%appeal}}', [
             'ap_id' => Schema::TYPE_PK . ' COMMENT \'Номер\'',
             'ap_created' => Schema::TYPE_DATETIME . ' COMMENT \'Создано\'',
-            'ap_finished' => Schema::TYPE_DATETIME . ' COMMENT \'Завершено\'',
+            'ap_next_act_date' => Schema::TYPE_DATETIME . ' COMMENT \'Срок действия\'', // ненулевое значение показывает, когда должно произойти следующее действие - ответ, проверка, назначение отвествтенных и т.д., когда нет даты, то нет дальнейших действий - обращение обработано
+            'ap_finished' => Schema::TYPE_DATETIME . ' COMMENT \'Завершено\'', // ненулевое значение здесь показывает, что обработка завершена
+
             'ap_pers_name' => Schema::TYPE_STRING . ' COMMENT \'Имя\'',
             'ap_pers_secname' => Schema::TYPE_STRING . ' COMMENT \'Отчество\'',
             'ap_pers_lastname' => Schema::TYPE_STRING . ' COMMENT \'Фамилия\'',
@@ -29,13 +31,15 @@ class m160524_073219_create_new_message_table extends Migration
             'ap_empl_id' => Schema::TYPE_INTEGER . ' COMMENT \'Исполнитель\'',
             'ap_curator_id' => Schema::TYPE_INTEGER . ' COMMENT \'Контролер\'',
             'ekis_id' => Schema::TYPE_INTEGER . ' COMMENT \'Учреждение\'',
-            'ap_state' => Schema::TYPE_INTEGER . ' COMMENT \'Состояние\'',
+
+            'ap_state' => Schema::TYPE_SMALLINT . ' Default 0 COMMENT \'Состояние\'',
+            'ap_is_archive' => Schema::TYPE_SMALLINT . ' Default 0 COMMENT \'В архиве\'', // здесь будем ставить флажок, чтобы не смотреть старые сообщения в выборках
         ], $tableOptionsMyISAM);
 
-        $this->createIndex('idx_ap_created', '{{%appeal}}', 'ap_created');
         $this->createIndex('idx_ap_empl_id', '{{%appeal}}', 'ap_empl_id');
         $this->createIndex('idx_ap_state', '{{%appeal}}', 'ap_state');
         $this->createIndex('idx_ap_ekis_id', '{{%appeal}}', 'ekis_id');
+        $this->createIndex('idx_ap_is_archive', '{{%appeal}}', 'ap_is_archive');
 
         $this->createTable('{{%answer}}', [
             'ans_id' => Schema::TYPE_PK . ' COMMENT \'Номер\'',
@@ -44,21 +48,23 @@ class m160524_073219_create_new_message_table extends Migration
             'ans_text' => Schema::TYPE_TEXT . ' COMMENT \'Ответ\'',
             'ans_remark' => Schema::TYPE_TEXT . ' COMMENT \'Замечание\'',
 
-            'ans_state' => Schema::TYPE_INTEGER . ' COMMENT \'Состояние\'',
+            'ans_state' => Schema::TYPE_SMALLINT . ' Default 0 COMMENT \'Состояние\'',
             'ans_ap_id' => Schema::TYPE_INTEGER . ' COMMENT \'Обращение\'',
             'ans_us_id' => Schema::TYPE_INTEGER . ' COMMENT \'Ответчик\'',
+            'ans_mark' => Schema::TYPE_SMALLINT . ' COMMENT \'Оценка ответа\'',
+            'ans_mark_comment' => Schema::TYPE_TEXT . ' COMMENT \'Комментарий к оценке\'',
         ], $tableOptionsMyISAM);
 
         $this->createIndex('idx_ans_ap_id', '{{%answer}}', 'ans_ap_id');
-        $this->createIndex('idx_ans_created', '{{%answer}}', 'ans_created');
-        $this->createIndex('idx_ans_state', '{{%answer}}', 'ans_state');
         $this->createIndex('idx_ans_us_id', '{{%answer}}', 'ans_us_id');
+        $this->createIndex('idx_ans_state', '{{%answer}}', 'ans_state');
 
         $this->refreshCache();
     }
 
     public function down()
     {
+        $this->dropTable('{{%answer}}');
         $this->dropTable('{{%appeal}}');
         $this->refreshCache();
     }
